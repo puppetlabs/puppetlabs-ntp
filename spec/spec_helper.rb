b78ee29465a20300aaa675a5867508c958c3896c
Dan Bode <dan@puppetlabs.com>
2011-11-14 22:24:01 -0800

    (#10846) add spec tests for ntp class
    This commit adds some unit tests for the
    ntp class.
    
    Also adds a rakefile for running the spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
-require 'pathname'
-dir = Pathname.new(__FILE__).parent
-$LOAD_PATH.unshift(dir, dir + 'lib', dir + '../lib')
-
-require 'mocha'
 require 'puppet'
-gem 'rspec', '=1.2.9'
-require 'spec/autorun'
+require 'rspec-puppet'
 
-Spec::Runner.configure do |config|
-    config.mock_with :mocha
-end
-
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
+RSpec.configure do |c|
+  puts File.join(File.dirname(__FILE__), '../../')
+  c.module_path = File.join(File.dirname(__FILE__), '../../')
 end
