6b6ccae923f177ed5da2e14cf755e78f5702a114
Ken Barber <ken@bob.sh>
2012-05-14 12:04:02 +0100

    (#11156) Fix module path fixtures so rspec works in most rubies
    Previously this module was using an experimental pattern using mktmpdir for
    creating its module path for rspec-puppet to use, which has since been
    deprecated in favour of a static directory. This was largely because ntp was
    one of the earlier modules we tried to make work in Jenkins.
    
    This change aligns the module to use the same pattern used in the rspec-
    puppet setup stage:
    
    https://github.com/rodjek/rspec-puppet/blob/master/lib/rspec-puppet/setup.rb
    
    This should stop the problems with Ruby 1.8.5 complaining about a lack of
    mktmpdir, and also make it more consistent with other modules in the wild.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,9 @@
 require 'puppet'
 require 'rspec-puppet'
-require 'tmpdir'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.before :each do
-    # Create a temporary puppet confdir area and temporary site.pp so
-    # when rspec-puppet runs we don't get a puppet error.
-    @puppetdir = Dir.mktmpdir("ntp")
-    manifestdir = File.join(@puppetdir, "manifests")
-    Dir.mkdir(manifestdir)
-    FileUtils.touch(File.join(manifestdir, "site.pp"))
-    Puppet[:confdir] = @puppetdir
-    Puppet.settings.send(:initialize_everything_for_tests) unless Puppet.version =~ /^2\.6/
-  end
-
-  c.after :each do
-    Puppet.settings.send(:clear_everything_for_tests) unless Puppet.version =~ /^2\.6/
-    FileUtils.remove_entry_secure(@puppetdir)
-  end
-
-  c.module_path = File.join(File.dirname(__FILE__), '../../')
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
