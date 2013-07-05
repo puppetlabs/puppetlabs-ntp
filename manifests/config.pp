c2df56745086606ba88f6824bd85211996a13662
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-05 17:55:16 -0400

    Add ntp::config, a class to manage the ntp configuration file.

---
+++ manifests/config.pp
@@ -0,0 +1,16 @@
+class ntp::config(
+  $config          = $ntp::config,
+  $config_template = $ntp::config_template,
+  $restrict        = $ntp::restrict,
+  $servers         = $ntp::servers,
+) {
+
+  file { $config:
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template("ntp/${config_template}"),
+  }
+
+}
