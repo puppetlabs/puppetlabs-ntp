b1bedbe26227680d47250e1f4c542384baa43982
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-09 16:06:03 -0400

    Improve the rspec-system tests, making sure we test for the package being installed and the config file at least makes basic sense.

--- spec/system/basic_spec.rb
+++ spec/system/basic_spec.rb
@@ -1,25 +1,13 @@
 require 'spec_helper_system'
 
-describe 'basic tests:' do
-  # Using puppet_apply as a subject
-  context puppet_apply 'notice("foo")' do
-    its(:stdout) { should =~ /foo/ }
-    its(:stderr) { should be_empty }
-    its(:exit_code) { should be_zero }
-  end
-
-  # Using puppet_apply as a helper
-  it 'my class should work with no errors' do
-    pp = <<-EOS
-      class { 'ntp': }
-    EOS
-
-    # Run it twice and test for idempotency
-    puppet_apply(pp) do |r|
-      r.exit_code.should_not == 1
-      r.refresh
-      r.exit_code.should be_zero
+# Here we put the more basic fundamental tests, ultra obvious stuff.
+describe "basic tests:" do
+  context 'make sure we have copied the module across' do
+    # No point diagnosing any more if the module wasn't copied properly
+    context shell 'ls /etc/puppet/modules/ntp' do
+      its(:stdout) { should =~ /Modulefile/ }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
     end
   end
 end
-
--- spec/system/ntp_install_spec.rb
+++ spec/system/ntp_install_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper_system'
 
+
 describe 'ntp::install class' do
   let(:os) {
     node.facts['osfamily']
   }
 
-  describe package('ntp') do
+  case node.facts['osfamily']
+  when 'FreeBSD'
+    packagename = 'net/ntp'
+  when 'Linux'
+    case node.facts['operatingsystem']
+    when 'ArchLinux'
+      packagename = 'ntp'
+    when 'Gentoo'
+      packagename = 'net-misc/ntp'
+    end
+  else
+    packagename = 'ntp'
+  end
+
+  puppet_apply(%{
+    class { 'ntp': }
+  })
+
+  describe package(packagename) do
     it { should be_installed }
   end
+
 end
