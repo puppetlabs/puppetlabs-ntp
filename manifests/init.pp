84a7d92edb24bf45108394782acc00a6c0f9436d
Matthew Haughton <matt@3flex.com.au>
2013-07-21 18:52:35 -0400

    correct anchor pattern bug number

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,7 +21,7 @@ class ntp (
   include '::ntp::config'
   include '::ntp::service'
 
-  # Anchor this as per #8140 - this ensures that classes won't float off and
+  # Anchor this as per #8040 - this ensures that classes won't float off and
   # mess everything up.  You can read about this at:
   # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
   anchor { 'ntp::begin': }
