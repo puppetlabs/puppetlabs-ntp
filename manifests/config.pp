7fbbbd0796caf0145b46887b55a7e2fd17a3ebb0
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-30 12:58:59 -0400

    Convert some classes to not be parameterized.
    We're following (where possible) the pattern of having parameters only
    exist on interface classes.  As ntp::config and ntp::install are just
    internal, private, classes, these don't make sense to allow changes to.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,18 @@
-class ntp::config (
-  $config            = $ntp::config,
-  $config_template   = $ntp::config_template,
-  $driftfile         = $ntp::driftfile,
-  $keys_enable       = $ntp::keys_enable,
-  $keys_file         = $ntp::keys_file,
-  $keys_controlkey   = $ntp::keys_controlkey,
-  $keys_requestkey   = $ntp::keys_requestkey,
-  $keys_trusted      = $ntp::keys_trusted,
-  $panic             = $ntp::panic,
-  $preferred_servers = $ntp::preferred_servers,
-  $restrict          = $ntp::restrict,
-  $servers           = $ntp::servers,
-) inherits ntp {
+#
+class ntp::config {
+
+  $config            = $ntp::config
+  $config_template   = $ntp::config_template
+  $driftfile         = $ntp::driftfile
+  $keys_enable       = $ntp::keys_enable
+  $keys_file         = $ntp::keys_file
+  $keys_controlkey   = $ntp::keys_controlkey
+  $keys_requestkey   = $ntp::keys_requestkey
+  $keys_trusted      = $ntp::keys_trusted
+  $panic             = $ntp::panic
+  $preferred_servers = $ntp::preferred_servers
+  $restrict          = $ntp::restrict
+  $servers           = $ntp::servers
 
   if $keys_enable {
     $directory = dirname($keys_file)
