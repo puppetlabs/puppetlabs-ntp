54ec7f94aa7068699e7a147f11ab954d825d2f3f
Marcus Rückert <mrueckert@suse.de>
2014-02-07 17:02:24 +0100

    add parameter to set a log file

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class ntp (
   $config            = $ntp::params::config,
   $config_template   = $ntp::params::config_template,
   $driftfile         = $ntp::params::driftfile,
+  $logfile           = $ntp::params::logfile,
   $keys_enable       = $ntp::params::keys_enable,
   $keys_file         = $ntp::params::keys_file,
   $keys_controlkey   = $ntp::params::keys_controlkey,
@@ -24,6 +25,7 @@ class ntp (
   validate_absolute_path($config)
   validate_string($config_template)
   validate_absolute_path($driftfile)
+  validate_absolute_path($logfile)
   validate_bool($keys_enable)
   validate_re($keys_controlkey, ['^\d+$', ''])
   validate_re($keys_requestkey, ['^\d+$', ''])
