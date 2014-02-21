61ff8929f5803dfbc698ce67e6e0844d6fa0b70c
Ashley Penney <ashley.penney@puppetlabs.com>
2014-02-21 22:14:28 +0000

    Revert "Workaround for stdlib 3.2 in PE3.x."
    This reverts commit 7472dfe677b399e187896f9a99c0ba4f508b46ea.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,8 +2,7 @@
 class ntp::config inherits ntp {
 
   if $keys_enable {
-    # Workaround for the lack of dirname() in stdlib 3.2.
-    $directory = inline_template('<%= File.dirname(keys_file) %>')
+    $directory = dirname($keys_file)
     file { $directory:
       ensure  => directory,
       owner   => 0,
