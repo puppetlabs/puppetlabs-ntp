da8162762f35a3dc447b0df424fab1f3dc14ae9b
Stanislaw Bogatkin <sbog@sbog.ru>
2015-04-09 12:44:11 +0300

    Add tinker, stepout and poll options, change panic option
    Change options:
    * panic - panic is not boolean, it is integer, actually
    
    Add options:
    * tinker - disable or enable tinker
    * minpoll - to change ntpd send packages frequency
    * maxpoll - to change ntpd send packages frequency
    * stepout - to change stepout interval
    
    Also Readme file changed accordingly.

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,12 +8,14 @@ class ntp::params {
   $keys_requestkey   = ''
   $keys_trusted      = []
   $logfile           = undef
+  $minpoll           = undef
   $package_ensure    = 'present'
   $peers             = []
   $preferred_servers = []
   $service_enable    = true
   $service_ensure    = 'running'
   $service_manage    = true
+  $stepout           = undef
   $udlc              = false
   $udlc_stratum      = '10'
   $interfaces        = []
@@ -22,14 +24,6 @@ class ntp::params {
 
   # Allow a list of fudge options
   $fudge             = []
-
-  # On virtual machines allow large clock skews.
-  # TODO Change this to str2bool($::is_virtual) when stdlib dependency is >= 4.0.0
-  # NOTE The "x${var}" is just to avoid lint quoted variable warning.
-  $panic = "x${::is_virtual}" ? {
-    'xtrue' => false,
-    default => true,
-  }
 
   $default_config       = '/etc/ntp.conf'
   $default_keys_file    = '/etc/ntp/keys'
@@ -40,6 +34,15 @@ class ntp::params {
   $package_manage = $::osfamily ? {
     'FreeBSD' => false,
     default   => true,
+  }
+
+  if str2bool($::is_virtual) {
+    $tinker = true
+    $panic  = 0
+  }
+  else {
+    $tinker = false
+    $panic  = undef
   }
 
   case $::osfamily {
@@ -60,6 +63,7 @@ class ntp::params {
         '2.debian.pool.ntp.org',
         '3.debian.pool.ntp.org',
       ]
+      $maxpoll       = undef
     }
     'Debian': {
       $config          = $default_config
@@ -80,6 +84,7 @@ class ntp::params {
         '2.debian.pool.ntp.org',
         '3.debian.pool.ntp.org',
       ]
+      $maxpoll         = undef
     }
     'RedHat': {
       $config          = $default_config
@@ -99,6 +104,7 @@ class ntp::params {
         '1.centos.pool.ntp.org',
         '2.centos.pool.ntp.org',
       ]
+      $maxpoll         = undef
     }
     'Suse': {
       if $::operatingsystem == 'SLES' and $::operatingsystemmajrelease == '12'
@@ -125,6 +131,7 @@ class ntp::params {
         '2.opensuse.pool.ntp.org',
         '3.opensuse.pool.ntp.org',
       ]
+      $maxpoll         = undef
     }
     'FreeBSD': {
       $config          = $default_config
@@ -140,11 +147,12 @@ class ntp::params {
       $service_name    = $default_service_name
       $iburst_enable   = true
       $servers         = [
-        '0.freebsd.pool.ntp.org maxpoll 9',
-        '1.freebsd.pool.ntp.org maxpoll 9',
-        '2.freebsd.pool.ntp.org maxpoll 9',
-        '3.freebsd.pool.ntp.org maxpoll 9',
+        '0.freebsd.pool.ntp.org',
+        '1.freebsd.pool.ntp.org',
+        '2.freebsd.pool.ntp.org',
+        '3.freebsd.pool.ntp.org',
       ]
+      $maxpoll         = 9
     }
     'Archlinux': {
       $config          = $default_config
@@ -164,6 +172,7 @@ class ntp::params {
         '1.pool.ntp.org',
         '2.pool.ntp.org',
       ]
+      $maxpoll         = undef
     }
     'Solaris': {
       $config        = '/etc/inet/ntp.conf'
@@ -187,6 +196,7 @@ class ntp::params {
         '2.pool.ntp.org',
         '3.pool.ntp.org',
       ]
+      $maxpoll       = undef
     }
   # Gentoo was added as its own $::osfamily in Facter 1.7.0
     'Gentoo': {
@@ -208,6 +218,7 @@ class ntp::params {
         '2.gentoo.pool.ntp.org',
         '3.gentoo.pool.ntp.org',
       ]
+      $maxpoll         = undef
     }
     'Linux': {
     # Account for distributions that don't have $::osfamily specific settings.
@@ -232,6 +243,7 @@ class ntp::params {
             '2.gentoo.pool.ntp.org',
             '3.gentoo.pool.ntp.org',
           ]
+          $maxpoll         = undef
         }
         default: {
           fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution.")
