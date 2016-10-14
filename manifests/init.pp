77fbf3f5e57b9727f00ca8efeca20023d09fa802
Helen Campbell <helen@puppetlabs.com>
2016-10-14 15:06:45 +0100

    Replacing anchor functionality with contain

--- manifests/init.pp
+++ manifests/init.pp
@@ -65,9 +65,12 @@ class ntp (
   # Anchor this as per #8040 - this ensures that classes won't float off and
   # mess everything up.  You can read about this at:
   # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
-  anchor { 'ntp::begin': } ->
-  class { '::ntp::install': } ->
-  class { '::ntp::config': } ~>
-  class { '::ntp::service': } ->
-  anchor { 'ntp::end': }
+
+  contain ntp::install
+  contain ntp::config
+  contain ntp::service
+
+  Class['::ntp::install'] ->
+  Class['::ntp::config'] ~>
+  Class['::ntp::service']
 }
