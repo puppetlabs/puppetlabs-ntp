21fb5f7844d27ea8deb9ae013114438fae6e934c
Dan Nawrocki <dnawrocki@athenasciences.com>
2017-03-17 11:29:45 -0400

    removing group/world readable permissions on keys file

--- manifests/config.pp
+++ manifests/config.pp
@@ -26,7 +26,7 @@ class ntp::config inherits ntp {
       ensure  => file,
       owner   => 0,
       group   => 0,
-      mode    => '0644',
+      mode    => '0600',
       content => template('ntp/keys.erb'),
     }
   }
