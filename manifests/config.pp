4b231225af575c11f10b7d1fb28bad6f9d8c1b11
tphoney <tp@puppet.com>
2017-07-10 09:47:20 +0100

    cleaning up opensuse default parameters

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,10 @@
-# @api private 
+# @api private
 # This class handles the configuration file. Avoid modifying private classes.
 class ntp::config inherits ntp {
 
   #The servers-netconfig file overrides NTP config on SLES 12, interfering with our configuration.
-  if $facts['operatingsystem'] == 'SLES' and $facts['operatingsystemmajrelease'] == '12' {
+  if ($facts['operatingsystem'] == 'SLES' and $facts['operatingsystemmajrelease'] == '12') or
+    ($facts['operatingsystem'] == 'OpenSuSE' and $facts['operatingsystemmajrelease'] == '42') {
     file { '/var/run/ntp/servers-netconfig':
       ensure => 'absent'
     }
