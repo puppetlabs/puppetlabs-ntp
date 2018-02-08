7e3ab69033eff0c0d4e8711eaad62bc60837deb5
Ryan Hall <ryan_hall@glic.com>
2018-02-08 16:09:29 -0500

    please appease the travis ci gods

--- spec/acceptance/disable_dhclient_spec.rb
+++ spec/acceptance/disable_dhclient_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper_acceptance'
 
+ntp_sh = '/etc/dhcp/dhclient.d/ntp.sh'
+ntp_conf_dhcp = '/var/lib/ntp/ntp.conf.dhcp'
+dhclient_conf = '/files/etc/dhcp/dhclient.conf'
+
 describe 'ntp class with disable_dhclient:', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
   context 'with should disable' do
     let(:pp) { "class { 'ntp': disable_dhclient => true }" }
@@ -9,19 +13,23 @@ describe 'ntp class with disable_dhclient:', unless: UNSUPPORTED_PLATFORMS.inclu
       apply_manifest(pp, catch_changes: true)
     end
 
-    describe file('/files/etc/dhcp/dhclient.conf') do
+    describe file(dhclient_conf.to_s) do
       its(:content) { is_expected.not_to match('ntp-servers') }
     end
 
-    it {
-      is_expected.to contain_file('/etc/dhcp/dhclient.d/ntp.sh').with(
+    describe file(ntp_sh.to_s) do
+      it { is_expected.to contain_file(ntp_sh.to_s).with(
         'ensure' => 'absent',
         'backup' => 'true',
-      )
-    }
+        )
+      }
+    end
 
-    it { is_expected.to contain_file('/var/lib/ntp/ntp.conf.dhcp').with('ensure' => 'absent') }
+    describe file(ntp_conf_dhcp.to_s) do
+      it { is_expected.to contain_file(ntp_conf_dhcp.to_s).with('ensure' => 'absent') }
+    end
   end
+
   context 'when enabled' do
     let(:pp) { "class { 'ntp': disable_dhclient => false }" }
 
@@ -29,8 +37,9 @@ describe 'ntp class with disable_dhclient:', unless: UNSUPPORTED_PLATFORMS.inclu
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
+
     describe file('/files/etc/dhcp/dhclient.conf') do
       its(:content) { is_expected.to match('ntp-servers') }
     end
-
+  end
 end
