6864b17037bc8c0793d3d34257b7be08e4259bab
Ryan Hall <ryan_hall@glic.com>
2018-02-08 15:29:29 -0500

    adjust testing for Travis CI

--- spec/acceptance/disable_dhclient_spec.rb
+++ spec/acceptance/disable_dhclient_spec.rb
@@ -13,15 +13,14 @@ describe 'ntp class with disable_dhclient:', unless: UNSUPPORTED_PLATFORMS.inclu
       its(:content) { is_expected.not_to match('ntp-servers') }
     end
 
-    it {
-      should contain_file('/etc/dhcp/dhclient.d/ntp.sh').with(
+    it { is_expected.to contain_file('/etc/dhcp/dhclient.d/ntp.sh').with(
         'ensure' => 'absent',
         'backup' => 'true',
       )
-      should contain_file('/var/lib/ntp/ntp.conf.dhcp').with(
-        'ensure' => 'absent',
-      )
     }
+
+    it { is_expected.to contain_file('/var/lib/ntp/ntp.conf.dhcp').with( 'ensure' => 'absent', ) }
+
   end
 
   context 'when enabled' do
@@ -35,5 +34,7 @@ describe 'ntp class with disable_dhclient:', unless: UNSUPPORTED_PLATFORMS.inclu
     describe file('/files/etc/dhcp/dhclient.conf') do
       its(:content) { is_expected.to match('ntp-servers') }
     end
+
   end
+
 end
