059365b1f78d351324105cd666fe5bc33afa947d
juju4 <juju4@users.noreply.github.com>
2019-03-22 23:17:38 -0400

    acceptance: review idempotence and set config based on serverspec, not facter

--- spec/acceptance/ntp_daemon_opts_spec.rb
+++ spec/acceptance/ntp_daemon_opts_spec.rb
@@ -1,22 +1,14 @@
 require 'spec_helper_acceptance'
 
-config = if fact('osfamily') == 'redhat'
+config = if os[:family] == 'redhat'
            '/etc/sysconfig/ntp'
          else
            '/etc/default/ntp'
          end
 
 describe 'ntp class with daemon options:', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
-  context 'when successful' do
-    let(:pp) { "class { 'ntp': user => 'ntp', daemon_extra_opts => '-g -i /var/lib/ntp' }" }
-
-    it 'runs twice' do
-      2.times do
-        apply_manifest(pp, catch_failures: true) do |r|
-          expect(r.stderr).not_to match(%r{error}i)
-        end
-      end
-    end
+  it 'run module twice idempotently' do
+    idempotent_apply(default, pp, {})
   end
 
   describe file(config.to_s) do
