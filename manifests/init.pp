47afa500116385b9c0e3193618ca6249301ddc13
Thomas Manninger <t.manninger@ixolit.com>
2020-11-23 13:14:06 +0100

    mode for logfile ntp

--- manifests/init.pp
+++ manifests/init.pp
@@ -89,7 +89,7 @@
 #   Specifies a log file for NTP to use instead of syslog. Default value: ' '.
 
 # @param logfile_mode
-#   Specifies the permission fot the NTP log file. Default is 0644.
+#   Specifies the permission fot the NTP log file. Default is 0664.
 #
 # @param logconfig
 #   Specifies the logconfig for NTP to use. Default value: ' '.
@@ -248,7 +248,7 @@ class ntp (
   Stdlib::Absolutepath $driftfile,
   Optional[Stdlib::Absolutepath] $leapfile,
   Optional[Stdlib::Absolutepath] $logfile,
-  Optional[String] $logfile_mode,
+  String $logfile_mode,
   Optional[String] $logconfig,
   Boolean $iburst_enable,
   Array[String] $keys,
