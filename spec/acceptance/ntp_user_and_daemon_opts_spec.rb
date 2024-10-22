0b195a2f666a0f15456ad801722bc77ee0c02e5a
Shubham Shinde <shubham.shinde@perforce.com>
2024-10-23 00:07:13 +0530

    (CAT-2100) Add Debian 12 support
    Adjust configs as per ntpsec which runs on Debian 12 in favour of ntp.

--- spec/acceptance/ntp_user_and_daemon_opts_spec.rb
+++ spec/acceptance/ntp_user_and_daemon_opts_spec.rb
@@ -17,6 +17,8 @@ when 'aix'
 else
   servicename = if os[:family] == 'sles' && os[:release].start_with?('12', '15')
                   'ntpd'
+                elsif os[:family] == 'debian' && os[:release].start_with?('12')
+                  'ntpsec'
                 else
                   'ntp'
                 end
@@ -25,11 +27,24 @@ config = if os[:family] == 'redhat'
            '/etc/sysconfig/ntpd'
          elsif os[:family] == 'sles'
            '/etc/sysconfig/ntp'
+         elsif os[:family] == 'debian' && os[:release].start_with?('12')
+           '/etc/ntpsec/ntp.conf'
          else
            '/etc/default/ntp'
          end
+
+if os[:family] == 'debian' && os[:release].start_with?('12')
+  ntpd_opts_match = %r{(OPTIONS|NTPD_OPTS)='-g -i /var/lib/ntpsec'}
+  chroot_dir = '/var/lib/ntpsec'
+else
+  ntpd_opts_match = %r{(OPTIONS|NTPD_OPTS)='-g -i /var/lib/ntp'}
+  chroot_dir = '/var/lib/ntp'
+end
+
 describe 'ntp class with daemon options:', unless: UNSUPPORTED_PLATFORMS.include?(os[:family]) || (os[:release].start_with?('5') && os[:family] == 'redhat') do
-  let(:pp) { "class { 'ntp': service_enable => true, service_ensure => running, service_manage => true, service_name => '#{servicename}', user => 'ntp', daemon_extra_opts => '-g -i /var/lib/ntp' }" }
+  let(:pp) do
+    "class { 'ntp': service_enable => true, service_ensure => running, service_manage => true, service_name => '#{servicename}', user => 'ntp', daemon_extra_opts => '-g -i #{chroot_dir}' }"
+  end
 
   context 'when run' do
     it 'is successful' do # rubocop:disable RSpec/NoExpectationExample
@@ -37,7 +52,7 @@ describe 'ntp class with daemon options:', unless: UNSUPPORTED_PLATFORMS.include
     end
 
     describe file(config.to_s) do
-      its(:content) { is_expected.to match(%r{(OPTIONS|NTPD_OPTS)='-g -i /var/lib/ntp'}) }
+      its(:content) { is_expected.to match(ntpd_opts_match) }
     end
 
     if os[:family] == 'redhat' && !os[:release].start_with?('6')
