2dc669f537dafeae2d9d64a8a3750f62097f1ead
Lukas Audzevicius <laudzevicius24@hotmail.com>
2025-12-17 15:23:08 +0000

    (CAT-2511-Troubleshoot_SLES_failures)

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,11 +6,13 @@
 class ntp::install {
   if $ntp::package_manage {
     if ($facts['os']['name'] == 'SLES' and $facts['os']['release']['major'] == '15') {
+      $sles_version = $facts['os']['release']['full']
       exec { 'Enable legacy repos':
         path    => '/bin:/usr/bin/:/sbin:/usr/sbin',
-        command => '/usr/bin/SUSEConnect --product sle-module-legacy/15.6/x86_64',
-        unless  => 'SUSEConnect --status-text | grep sle-module-legacy/15.6/x86_64',
+        command => "/usr/bin/SUSEConnect --product sle-module-legacy/${sles_version}/x86_64",
+        unless  => "SUSEConnect --status-text | grep 'sle-module-legacy/${sles_version}/x86_64'",
       }
+      -> Package[$ntp::package_name]
     }
 
     package { $ntp::package_name:
